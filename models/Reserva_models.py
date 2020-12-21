7901ac4105d1489f01095289bf0c8dc322fde75c
Karen Daniela Gómez Aldana <kadagoal@gmail.com>
2020-12-21 22:19:35 +0100

    Primera entrega

---
+++ models/Reserva_models.py
@@ -0,0 +1,14 @@
+from datetime import date
+from pydantic import BaseModel
+class ReservaIn(BaseModel):
+    Nombre: str
+    Destino: str
+    Hotel: str
+class ReservaOut(BaseModel):
+    Nombre: str
+    Destino: str
+    Hotel: str
+    Habitaciones: int
+    Valor: int
+    Fecha_entrada: date
+    Fecha_salida: date
